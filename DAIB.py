04344c16899e0c7b665538b514edf9eef2858789
Gautham Nair <gautham.nair.2005@outlook.com>
2023-11-13 18:18:41 +0530

    Update DAIB.py

--- DAIB.py
+++ DAIB.py
@@ -37,7 +37,7 @@ llm.temperature=0.7
 engine = pyttsx3.init()
 engine.setProperty('rate',150)
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
 
 
 def speak(audio):                   
