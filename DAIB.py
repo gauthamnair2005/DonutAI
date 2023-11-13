ab0d4b86b2d093720ec0db91cd84ffff608880eb
Gautham Nair <gautham.nair.2005@outlook.com>
2023-11-13 19:38:08 +0530

    Update GUI

--- DAIB.py
+++ DAIB.py
@@ -23,7 +23,7 @@ import threading
 root = Tk()
 root.title("DonutAI")
 
-BG_GRAY = "#ABB2B9"
+BG_GRAY = "#ABB2B0"
 BG_COLOR = "#17202A"
 TEXT_COLOR = "#EAECEE"
 
@@ -39,6 +39,74 @@ engine.setProperty('rate',150)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+def takeCommand():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		r.pause_threshold = 1
+		audio = r.listen(source)
+	try:
+		txt.insert(END,"\n" + "Recognizing...\n")    
+		user = r.recognize_google(audio, language='en-in')
+		txt.insert(END,"\n" + "You : " + user+ "\n")
+		if (user == "hello"):
+			txt.insert(END, "\n" + "DonutAI : Hi there, how can I help?" + "\n")
+			res_str = "Hi there, how can I help?"
+			t = threading.Thread(target=speak, args=(res_str,))
+			t.start()
+
+		elif (user == "hi" or user == "hii" or user == "hiiii"):
+			txt.insert(END, "\n" + "DonutAI : Hi there, what can I do for you?" + "\n")
+			res_str = "Hi there, how can I help?"
+			t = threading.Thread(target=speak, args=(res_str,))
+			t.start()
+		elif (user == "exit" or user == "Exit"):
+			root.quit()
+		elif (user == "how are you"):
+			txt.insert(END, "\n" + "DonutAI : I'm fine! and you" + "\n")
+			res_str = "I'm fine! and you?"
+			t = threading.Thread(target=speak, args=(res_str,))
+			t.start()
+		elif (user == "fine" or user == "i am good" or user == "i am doing good"):
+			txt.insert(END, "\n" + "DonutAI : Great! how can I help you?" + "\n")
+			res_str = "Great! how can I help you?"
+			t = threading.Thread(target=speak, args=(res_str,))
+			t.start()
+		elif (user == "thanks" or user == "thank you" or user == "now its my time"):
+			txt.insert(END, "\n" + "DonutAI : My pleasure !" + "\n")
+			res_str = "My pleasure !"
+			t = threading.Thread(target=speak, args=(res_str,))
+			t.start()
+		elif (user == "tell me a joke" or user == "tell me something funny" or user == "crack a funny line"):
+			My_joke = pyjokes.get_joke(language="en", category="neutral")
+			res_str = ''.join(map(str, My_joke))    
+			txt.insert(END, "\n" + "DonutAI : " + res_str + "\n")
+			t = threading.Thread(target=speak, args=(res_str,))
+			t.start()
+		elif (user == "" or user == " "):
+			pass
+		elif (user == "goodbye" or user == "see you later" or user == "see yaa"):
+			root.quit()
+
+		else:
+			try:
+				prompt = [user]
+				llm_results = llm._generate(prompt)
+				res = llm_results.generations
+				res_str = ''.join(map(str, res[0][0].text))
+				txt.insert(END,"\n")
+				txt.insert(END,"\n" + "Generating Answers..!")
+				txt.insert(END, "\n")
+				txt.insert(END, "\n" + "DonutAI : " + res_str)
+				txt.insert(END, "\n")
+				t = threading.Thread(target=speak, args=(res_str,))
+				t.start()
+			except:
+				txt.insert(END,"\n" + "An unexpected Error Occurred.!")
+	except Exception as e:  
+		txt.insert(END,"\n Say that again please... \n")
+		speak('I didnt hear anything, if you said anything please speak loud and clear')
+		return ""
+	return user
 
 def speak(audio):                   
     engine.say(audio)
@@ -62,6 +130,8 @@ def send():
 		res_str = "Hi there, how can I help?"
 		t = threading.Thread(target=speak, args=(res_str,))
 		t.start()
+	elif (user == "exit" or user == "Exit"):
+		root.quit()
 	elif (user == "how are you"):
 		txt.insert(END, "\n" + "DonutAI : I'm fine! and you" + "\n")
 		res_str = "I'm fine! and you?"
@@ -86,7 +156,6 @@ def send():
 	elif (user == "" or user == " "):
 		pass
 	elif (user == "goodbye" or user == "see you later" or user == "see yaa"):
-		txt.insert(END, "\n" + "DonutAI : Have a nice day!" + "\n")
 		root.quit()
 
 	else:
@@ -106,17 +175,17 @@ def send():
 			txt.insert(END,"\n" + "An unexpected Error Occurred.!")
 	e.delete(0, END)
 
-lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="🍩 DonutAI PREVIEW", font=FONT_BOLD, pady=12, width=60, height=1).grid(
-    row=0)
-txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="🍩 DonutAI PREVIEW", font=FONT_BOLD, pady=0, width=65, height=1).grid(row=0,column=0,columnspan=2)
+txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, pady=0, font=FONT, width=60)
 txt.grid(row=1, column=0, columnspan=2)
 scrollbar = Scrollbar(txt)
-scrollbar.place(relheight=1, relx=0.974)
+scrollbar.place(relheight=1, relx=1)
 
 e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
 e.grid(row=2, column=0)
 
-send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
-			command=send).grid(row=2, column=1)
+send = Button(root, text="➡️Send", font=FONT_BOLD, bg=BG_GRAY,command=send).grid(row=2, column=1)
+voice_input = Button(root, text="🎙️Mic.", font=FONT_BOLD, bg=BG_GRAY, command=takeCommand)
+voice_input.grid(row=2, column=2)
 
 root.mainloop()
