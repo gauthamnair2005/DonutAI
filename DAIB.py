1b6c408cf8006c099f0daa16989523c7136614fc
Gautham Nair <gautham.nair.2005@outlook.com>
2023-12-04 17:14:02 +0530

    modified:   DAIB.py

--- DAIB.py
+++ DAIB.py
@@ -6,10 +6,13 @@ import speech_recognition as sr
 from PyQt5.QtWidgets import QApplication, QSizePolicy,QTextEdit,QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QTextBrowser, QLabel
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
 from langchain.llms import GooglePalm
 import html
 import time
 import wolframalpha
+import json
 
 donutai_keywords = ["DonutAI", "donutai", "Donutai", "donutAI", "Donut AI", "donut ai", "donutAi", "donut AI"]
 developer_keywords = ["who developed you", "who developed you?", "who created you", "who created you?", "who made you", "who made you?", "who is your developer", "who is your developer?", "who is your creator", "who is your creator?", "who is your father", "who is your father?", "who is your dad", "who is your dad?", "who is your daddy", "who is your daddy?", "Who developed you", "Who developed you?", "Who created you", "Who created you?", "Who made you", "Who made you?", "Who is your developer", "Who is your developer?", "Who is your creator", "Who is your creator?", "Who is your father", "Who is your father?", "Who is your dad", "Who is your dad?", "Who is your daddy", "Who is your daddy?"]
@@ -23,7 +26,20 @@ class ChatbotGUI(QWidget):
     def __init__(self):
         super().__init__()
         self.stop_flag = False
-        self.chat_history = QTextEdit()
+        self.chat_history = QWebEngineView()
+        self.chat_history.setHtml("""
+            <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial;
+                        font-size: 20px;
+                    }
+                </style>
+            </head>
+            <body></body>
+            </html>
+        """)
         self.message_entry = QLineEdit()
         self.send_button = QPushButton('Send')
         self.send_button.setStyleSheet("QPushButton {background-color: orange; border-radius: 30%; padding: 15px; font-size : 20px} QPushButton:pressed {background-color: white;}")
@@ -58,15 +74,14 @@ class ChatbotGUI(QWidget):
         self.voice_button.setFixedSize(60, 60)
 
         # Set styles
-        self.chat_history.setStyleSheet("font-size: 20px; color: blue; background-color: lightgray;")
-        self.message_entry.setStyleSheet("font-size: 16px; color: green; background-color: lightgray;")
+        self.chat_history.setStyleSheet("font-size: 20px; color: blue;background-color: black;")
+        self.message_entry.setStyleSheet("font-size: 16px; color: darkviolet; background-color: lightgray;")
 
         self.chat_history.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         # Set the size policy of the prompt box
         self.message_entry.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.chat_history.setReadOnly(True)
         self.message_entry.setFocus()
 
         # Decrease the size of the buttons and place them beside the prompt box
@@ -89,36 +104,70 @@ class ChatbotGUI(QWidget):
         # Connect signals and slots
         self.send_button.clicked.connect(self.send)
         self.voice_button.clicked.connect(self.get_voice_input)
-        self.chat_history.setReadOnly(True)
+
+    def speak(self, text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+        
+
+    def append_to_chat_history(self, text):
+        escaped_text = json.dumps(text)
+        self.chat_history.page().runJavaScript("document.body.innerHTML += " + escaped_text + ";")
+
+    def wolf(self, msg):
+        try:
+            client = wolframalpha.Client('UL8UPY-4EHX5683WH')
+            res = client.query(msg)
+            response = next(res.results).text
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : </p>")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>"+response+"</p>")
+            self.append_to_chat_history("")
+            self.message_entry.clear()
+            threading.Thread(target=self.speak, args=(response,)).start()
+        except:
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : </p>")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>Could not give response </p>")
+            self.append_to_chat_history("")
+            self.message_entry.clear()
 
     def send(self):
         msg = self.message_entry.text()
         if msg == "":
-            self.chat_history.append("<p style='color=orange;'>DonutAI : Prompt cannot be empty.</p>")
-            self.chat_history.append("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : Prompt cannot be empty.</p>")
+            self.append_to_chat_history("")
             self.message_entry.clear()
             return
+        elif 'weather' in msg:
+            self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>You : </p>")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>"+msg+"</p>")
+            self.append_to_chat_history("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Fetching Latest Weather..!</p>")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Generating Answers..!</p>")
+            self.append_to_chat_history("")
+            self.message_entry.clear()
+            threading.Thread(target=self.wolf, args=(msg,)).start()
         elif msg == "exit":
-            self.chat_history.append("<p style='color:orange;'>DonutAI : Bye! You can press the 'X' or close button to close the window.</p>")
-            self.chat_history.append("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : Bye! You can press the 'X' or close button to close the window.</p>")
+            self.append_to_chat_history("")
             self.message_entry.clear()
             threading.Thread(target=self.speak, args=("Bye! You can press the 'X' or colese button to close the window.",)).start()
         elif msg in donutai_keywords:
-            self.chat_history.append("<p style='color:orange;'>DonutAI : Hi! I am DonutAI. I am a chatbot created by Gautham Nair. I am still in development, so please forgive me if I make any mistakes. </p>")
-            self.chat_history.append("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : Hi! I am DonutAI. I am a chatbot created by Gautham Nair. I am still in development, so please forgive me if I make any mistakes.</p>")
+            self.append_to_chat_history("")
             self.message_entry.clear()
             threading.Thread(target=self.speak, args=("Hi! I am DonutAI. I am a chatbot created by Gautham Nair. I am still in development, so please forgive me if I make any mistakes.",)).start()
         elif msg in developer_keywords:
-            self.chat_history.append("<p style='color:orange;'>DonutAI : I was developed by Gautham Nair.</p>")
-            self.chat_history.append("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : I was developed by Gautham Nair.</p>")
+            self.append_to_chat_history("")
             self.message_entry.clear()
             threading.Thread(target=self.speak, args=("I was developed by Gautham Nair.",)).start()
         else:
-            self.chat_history.append("<p style='color:darkviolet;'>You : </p>")
-            self.chat_history.append("<p style='color:darkviolet;'>"+msg+"</p>")
-            self.chat_history.append("")
-            self.chat_history.append("<p style='color:green;'>Generating Answers..!</p>")
-            self.chat_history.append("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>You : </p>")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>"+msg+"</p>")
+            self.append_to_chat_history("")
+            self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Generating Answers..!</p>")
+            self.append_to_chat_history("")
             self.message_entry.clear()
             threading.Thread(target=self.generate_response, args=(msg,)).start()
 
@@ -126,45 +175,46 @@ class ChatbotGUI(QWidget):
         def record_and_process():
             r = sr.Recognizer()
             with sr.Microphone() as source:
-                self.chat_history.append("<p style='color:green;'>Listening..!</p>")
-                self.chat_history.append("")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Listening..!</p>")
+                self.append_to_chat_history("")
                 audio = r.listen(source)
             try:
                 msg = r.recognize_google(audio)
                 if msg == "":
-                    self.chat_history.append("<p style='color:orange;'>DonutAI : Prompt cannot be empty.</p>")
-                    self.chat_history.append("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : Prompt cannot be empty.</p>")
+                    self.append_to_chat_history("")
                     self.message_entry.clear()
                     return
                 elif 'weather' in msg:
-                    self.chat_history.append("<p style='color:darkviolet;'>DonutAI : </p>")
-                    self.chat_history.append("<p style='color:darkviolet;'>"+msg+"</p>")
-                    self.chat_history.append("")
-                    self.chat_history.append("<p style='color:green;'>Generating Answers..!</p>")
-                    self.chat_history.append("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>You : </p>")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>"+msg+"</p>")
+                    self.append_to_chat_history("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Fetching Latest Weather..!</p>")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Generating Answers..!</p>")
+                    self.append_to_chat_history("")
                     self.message_entry.clear()
                     threading.Thread(target=self.wolf, args=(msg,)).start()
                 elif msg == "exit":
-                    self.chat_history.append("<p style='color:orange;'>DonutAI : Bye! You can press the 'X' or close button to close the window.</p>")
-                    self.chat_history.append("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : Bye! You can press the 'X' or close button to close the window.</p>")
+                    self.append_to_chat_history("")
                     self.message_entry.clear()
                     threading.Thread(target=self.speak, args=("Bye! You can press the 'X' or colese button to close the window.",)).start()
                 elif msg in donutai_keywords:
-                    self.chat_history.append("<p style='color:orange;'>DonutAI : Hi! I am DonutAI. I am a chatbot created by Gautham Nair. I am still in development, so please forgive me if I make any mistakes.</p>")
-                    self.chat_history.append("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : Hi! I am DonutAI. I am a chatbot created by Gautham Nair. I am still in development, so please forgive me if I make any mistakes.</p>")
+                    self.append_to_chat_history("")
                     self.message_entry.clear()
                     threading.Thread(target=self.speak, args=("Hi! I am DonutAI. I am a chatbot created by Gautham Nair. I am still in development, so please forgive me if I make any mistakes.",)).start()
                 elif msg in developer_keywords:
-                    self.chat_history.append("<p style='color:orange;'>DonutAI : I was developed by Gautham Nair.</p>")
-                    self.chat_history.append("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : I was developed by Gautham Nair.</p>")
+                    self.append_to_chat_history("")
                     self.message_entry.clear()
                     threading.Thread(target=self.speak, args=("I was developed by Gautham Nair.",)).start()
                 else:
-                    self.chat_history.append("<p style='color:darkviolet;'>You : </p>")
-                    self.chat_history.append("<p style='color:darkviolet;'>"+msg+"</p>")
-                    self.chat_history.append("")
-                    self.chat_history.append("<p style='color:green;'>Generating Answers..!</p>")
-                    self.chat_history.append("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>You : </p>")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:right;color:darkviolet;'>"+msg+"</p>")
+                    self.append_to_chat_history("")
+                    self.append_to_chat_history("<p style='font-family: arial; text-align:center;color:green;'>Generating Answers..!</p>")
+                    self.append_to_chat_history("")
                     self.message_entry.clear()
                     threading.Thread(target=self.generate_response, args=(msg,)).start()
             except sr.UnknownValueError:
@@ -175,7 +225,7 @@ class ChatbotGUI(QWidget):
                 return ""
             
         threading.Thread(target=record_and_process).start()
-
+    
     def generate_response(self, msg):
         msg = [msg]
         llm_results= llm._generate(msg)
@@ -190,15 +240,15 @@ class ChatbotGUI(QWidget):
             if response_text.startswith('```') and response_text.endswith('```'):
                 # Remove the backticks and wrap the response in a <pre> tag
                 response_text = '<pre>' + response_text[3:-3] + '</pre>'
-                self.chat_history.append("<p style='color:orange;'>DonutAI : </p>")
-                self.chat_history.append("<p style='color:orange;'>"+response_text+"</p>")
-                self.chat_history.append("")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : </p>")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>"+response_text+"</p>")
+                self.append_to_chat_history("")
                 self.message_entry.clear()
             else:
                 response_text = response_text.replace('&quot;', '"')
-                self.chat_history.append("<p style='color:orange;'>DonutAI : </p>")
-                self.chat_history.append("<p style='color:orange;'>"+response_text+"</p>")
-                self.chat_history.append("")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : </p>")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>"+response_text+"</p>")
+                self.append_to_chat_history("")
                 self.message_entry.clear()
 
             engine.say(response_text)
@@ -208,18 +258,17 @@ class ChatbotGUI(QWidget):
                 client = wolframalpha.Client('UL8UPY-4EHX5683WH')
                 res = client.query(msg)
                 response = next(res.results).text
-            except:
-                self.chat_history.append("<p style='color:orange;'>DonutAI : </p>")
-                self.chat_history.append("<p style='color:orange;'>Could not give response </p>")
-                self.chat_history.append("")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : </p>")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>"+response+"</p>")
+                self.append_to_chat_history("")
                 self.message_entry.clear()
-
+                threading.Thread(target=self.speak, args=(response,)).start()
+            except:
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>DonutAI : </p>")
+                self.append_to_chat_history("<p style='font-family: arial; text-align:left;color:orange;'>Could not give response </p>")
+                self.append_to_chat_history("")
+                self.message_entry.clear()
         
-    def wolf(self, msg):
-        client = wolframalpha.Client('UL8UPY-4EHX5683WH')
-        res = client.query(msg)
-        response = next(res.results).text
-        return response
 
 app = QApplication(sys.argv)
 window = ChatbotGUI()
