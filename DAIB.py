4a4ed08885277ed15204df44d07778c81cbca606
Gautham Nair <gautham.nair.2005@outlook.com>
2023-12-06 13:19:25 +0530

    Remove unnecessary import statement

--- DAIB.py
+++ DAIB.py
@@ -9,7 +9,6 @@ from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 from langchain.llms import GooglePalm
-import html
 import time
 import wolframalpha
 import json
