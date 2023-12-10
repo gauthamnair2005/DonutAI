9cd1fbe111ae8c79341337aa40eb3e07301f1185
Gautham Nair <gautham.nair.2005@outlook.com>
2023-12-10 16:44:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "DonutAI",
-    version = "1.0",
+    version = "23.12.10",
     description = "DonutAI Assistant",
-    executables = [Executable("DAIB.py", base = "Win32GUI")]
+    executables = [Executable("DonutAI.py", base = "Win32GUI")]
 )
